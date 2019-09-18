77133a9544c5f790ff2e6c3a95d74f14450799ed
AlexVermil <55434374+AlexVermil@users.noreply.github.com>
2019-09-18 11:46:17 +0300

    Add files via upload

---
+++ Yl 3.py
@@ -0,0 +1,10 @@
+a = int(input("Esimene : "))
+b = int(input("Teine : "))
+
+if a > b:
+    minimum = b
+    
+else:
+    minimum = a
+    
+print("Miinimum on: " + str(minimum))
---
+++ Yl 4.py
@@ -0,0 +1,11 @@
+a = int(input("Esimene : "))
+b = int(input("Teine : "))
+c = int(input("Kolmas : "))
+
+if a > b and a > c:
+    maximum = a
+elif b > c:
+    maximum = b
+else:
+    maximum = c
+print("Maksimum on: " + str(maximum))
